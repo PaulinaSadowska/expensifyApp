82288d90d6b58c8d54f2e72e5e1804988cf7bb3c
palka <7paulina.sadowska@gmail.com>
2021-08-24 20:14:15 +0200

    Private only routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 export const Header = ({ startLogout }) => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/" exact={true} activeClassName="is-active">Home page</NavLink>
+        <NavLink to="/dashboard" exact={true} activeClassName="is-active">Home page</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create expense</NavLink>
         <button onClick={startLogout}>Logout</button>
     </header>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,19 +7,18 @@ import LoginPage from '../components/LoginPage';
 import EditExpensePage from '../components/EditExpensePage';
 import { HelpPage } from '../components/HelpPage';
 import { PageNotFound } from '../components/PageNotFound';
-import Header from '../components/Header';
+import PrivateRoute from './PrivateRoute'
 
 export const history = createBrowserHistory()
 
 export const AppRouter = () => (
     <Router history={history}>
         <div>
-            <Header />
             <Switch>
                 <Route path="/" exact={true} component={LoginPage} />
-                <Route path="/dashboard" exact={true} component={ExpenseDashboardPage} />
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit/:id" component={EditExpensePage} />
+                <PrivateRoute path="/dashboard" exact={true} component={ExpenseDashboardPage} />
+                <PrivateRoute path="/create" component={AddExpensePage} />
+                <PrivateRoute path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
                 <Route component={PageNotFound}></Route>
             </Switch>
